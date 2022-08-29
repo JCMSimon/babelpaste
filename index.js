de8f8a12b4cb0a456ded8df0936239e07092f7a1
Simon <simon1240.se@gmail.com>
2022-08-29 16:57:31 +0200

    i hate JS

--- index.js
+++ index.js
@@ -7,70 +7,100 @@ const textField = document.getElementById('textField')
 // used to signal errors
 const alertText = document.getElementById("alertText")
 
+const allowedChars = "abcdefghijklmnopqrstuvwxyz ,."
+
+let finishedPasteText
+
 attachEventListeners();
 
 function submitButtonFunction() {
-	console.log("Do actual paste logic here");
 	clearAlert();
 	deactivateButtons();
 	let rawPasteText = getTextFromField();
-	// test if rawPasteText is empty
-	// if true
-	// 		setAlert("No Text found");
-	// 		activateButtons();
-	// if false
-	// 	teset if rawPasteText is empty after conversion (quick test by scanning for normal chars)
-	// if true
-	// 		setAlert("No Text found after conversion");
-	// 		activateButtons();
-	// if false
+	console.log(rawPasteText)
+	if (rawPasteText == "") {
+		setAlert("No Text found");
+		activateButtons();
+		return;
+	} else {
+		let counter = 0;
+		[...allowedChars].forEach(char => {
+			if (rawPasteText.includes(char)) {
+				counter += 1
+				}
+			}
+		)
+			if (counter == 0) {
+				console.log(counter)
+				setAlert("No Text found after conversion");
+				activateButtons();
+				return;
+			}
+		}
+	if (rawPasteText.length > 3200) {
+		setAlert("Text too long");
+		activateButtons();
+		return;
+	}
 	clearTextField();
 	try {
-		let finishedPasteText = convertPaste(rawPasteText)
+		finishedPasteText = convertPaste(rawPasteText)
 	}
-	catch {
-		setAlert("smth went wrong (contact info here)")
+	catch (e)  {
+		console.log(e.message);
+		setAlert("Smth went wrong (JCMS#0557)")
 		activateButtons();
 	}
+	console.log(finishedPasteText)
 	// check if text is over 3200 chars
 	// if true
 	//		setAlert("Paste too long after conversion")
 	// 		setTextField(rawPasteText)
 	// if false
-	try {
-		let hex, wall, shelf, volume, page = postSearchRequest(finishedPasteText);
-	}
-	catch {
-		setAlert("smth went wrong (contact info here)")
-		activateButtons();
-	}
-	try {
-		let title = postBookmarkRequest(hex, wall, shelf, volume, page)
-	}
-	catch {
-		setAlert("smth went wrong (contact info here)")
-		activateButtons();
-	}
+	// try {
+	// 	let hex, wall, shelf, volume, page = postSearchRequest(finishedPasteText);
+	// }
+	// catch {
+	// 	setAlert("smth went wrong (contact info here)")
+	// 	activateButtons();
+	// }
+	// try {
+	// 	let title = postBookmarkRequest(hex, wall, shelf, volume, page)
+	// }
+	// catch {
+	// 	setAlert("smth went wrong (contact info here)")
+	// 	activateButtons();
+	// }
 
-	let finalLink = `https://libraryofbabel.info/bookmark.cgi?${title}`
-	setTextField(finalLink);
+	// let finalLink = `https://libraryofbabel.info/bookmark.cgi?${title}`
+	// setTextField(finalLink);
 	activateButtons();
 }
 
 function convertPaste(rawpasteText) {
-	console.log("later")
+	let mapping = {".in.":"9",".ig.":"8",".ev.":"7",".x.":"6",".iv.":"5",".ou.":"4",".re.":"3",".t.":"2",".n.":"1",".er.":"0",".ex.":"!",".es.":"?",".qu.":'"',".pa.":"§",".do.":"$",".pe.":"%",".a.":"&",".eq.":"=",".fs.":"/",".bs.":"\\",".bo.":"(",".bc.":")",".cbc.":"}",".cbo.":"{",".sbc.":"]",".sbo.":"[",".ca.":"^",".de.":"°",".ls.":"<",".gt.":">",".or.":"|",".se.":";",".co.":":",".mi.":"-",".un.":"_",".ha.":"#",".sq.":"'",".fa.":"´",".fb.":"`",".pl.":"+",".st.":"*",".ti.":"~"};
+	var result = ""
+	for(let key in mapping) {
+		var value = mapping[key];
+		console.log(`Replacing ${value} with ${key}`);
+		result = rawpasteText.replace(`${value}`,`${key}`)
+		console.log(result)
+	  }
+	  return result
 }
 
 function deactivateButtons() {
-	console.log("idk")
+	submitButton.disabled = true
+	infoButton.disabled = true
 }
 
 function activateButtons() {
-	console.log("idk")
+	submitButton.disabled = false
+	infoButton.disabled = false
 }
 
 function getTextFromField() {
-	console.log("idk")
+	return textField.value
 }
 
 function postSearchRequest(term) {
@@ -82,19 +112,19 @@ function postBookmarkRequest(hex, wall, shelf, volume, page, title = "") {
 }
 
 function clearTextField() {
-	textField.innerText = ""
+	textField.value = ""
 }
 
 function setTextField(text) {
-	textField.innerText = `${text}`
+	textField.value = `${text}`
 }
 
 function setAlert(alertMessage) {
-	alertText.innerHTML = `${alertMessage}`
+	alertText.innerText = `${alertMessage}`
 }
 
 function clearAlert() {
-	alertText.innerHTML = ""
+	alertText.innerText = "­"
 }
 
 function attachEventListeners() {
